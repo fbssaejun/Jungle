adf0f024e6435f65ea65bf6ec5bb42f9a9cda33d
Anthony Kim <fbssaejun@gmail.com>
2021-09-15 19:31:41 +0000

    add http basic authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["USER_NAME"], password: ENV["PASSWORD"]
+
   def show
     @num_product = Product.count
     @num_categories = Category.count
