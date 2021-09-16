9f2dd6d8ae45f4f524449154cc1f276ee91b1b35
Anthony Kim <fbssaejun@gmail.com>
2021-09-16 16:51:36 +0000

    add user params method to collect user data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,14 @@ class UsersController < ApplicationController
     else
       redirect_to '/signup'
     end
+    
+  end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
 
-end
+
