9eb060d0ef34d29f061e7d50a2308fa103e6ccfa
Anthony Kim <fbssaejun@gmail.com>
2021-09-16 15:50:53 +0000

    syntax error fix

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,10 @@ Rails.application.routes.draw do
       # This is optional (as is the root to: above).
       get '/cool' => 'gif#cool'
       get '/sweet' => 'gif#sweet'
+
+      get '/login' => 'sessions#new'
+      post '/login' => 'sessions#create'
+      get '/logout' => 'sessions#destroy'
   
       # These routes will be for signup. The first renders a form in the browse, the second will 
       # receive the form and create a user in our database using the data given to us by the user.
