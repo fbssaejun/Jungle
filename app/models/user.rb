73cc8bfa3468a8301b5e1fde92e19b3058aa2926
Anthony Kim <fbssaejun@gmail.com>
2021-09-20 22:06:29 +0000

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :email, uniqueness: true
-  validates :password, length: {minimum : 5}
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 5}
+  validates :password_confirmation, presence: true
 end
