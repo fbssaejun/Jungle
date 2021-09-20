192e96389cae459267173086a6a7f06cecb77306
Anthony Kim <fbssaejun@gmail.com>
2021-09-20 23:33:31 +0000

    transform user's email to lowercase for edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 5}
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email.strip.downcase)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
 end
